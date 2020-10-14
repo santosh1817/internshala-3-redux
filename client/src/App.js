462ad6ec1f632503ebc06862b1c52ace313828ab
santosh1817 <santosh01994@gmail.com>
2020-10-15 02:12:22 +0530

    added display logic from redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Feedback from './components/Feedback'
 import './App.css';
-
+import {BrowserRouter,Link,Route,Switch} from 'react-router-dom';
+import Display from './components/display'
 import store from './store';
 import { Provider } from 'react-redux';
+
 
 class App extends React.Component
 {
@@ -11,10 +13,17 @@ class App extends React.Component
   render(){
     return(
       <Provider store={store}>
-      <div>
-       
-        <Feedback/>
-      </div>
+        <BrowserRouter>
+          <div>
+        
+            
+            <Switch>
+              <Route path="/form/submit" exact={true} component={ Display } />   
+              <Route path="/form" exact={true} component={ Feedback } /> 
+            </Switch>
+          </div>
+        </BrowserRouter>
+      
       </Provider>
     )
   }
--- client/src/components/display.js
+++ client/src/components/display.js
@@ -1,8 +1,21 @@
 import React ,{useState} from 'react'
+import { useSelector } from 'react-redux';
 
 const Display=()=>{
 
-    
+    const form = useSelector(state => state.user.form);
+
+    return(
+        <div>
+            <h1>Hello {form.name}</h1>  
+            <h1>Hello {form.email}</h1>  
+            <h1>Hello {form.mobile}</h1>  
+            <h1>Hello {form.rating}</h1> 
+            
+
+        </div>
+       
+    )    
 
 }
 export default Display
